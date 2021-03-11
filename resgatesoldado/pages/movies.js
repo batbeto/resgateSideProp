fdad82257cf8d58a3090e44d9cdf0439bf65daff
Adalberto Maia <adalberto@objetorelacional.com.br>
2021-03-10 21:07:59 -0300

    resgate done

---
+++ resgatesoldado/pages/movies.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const fetchData = async () => await axios.get(`http://www.omdbapi.com/?apikey=fd6b0637&s=resgate`)
+  .then(res => ({
+    error: false,
+    films: res.data,
+  }))
+  .catch(() => ({
+      error: true,
+      films: null,
+    }),
+  );
+
+const Films = ({ films, error }) => {
+  return (
+    <div>
+        { films.Search.map( (m) => <div>{m.Title} --- <br /><img src={m.Poster} alt="POSTER FALTANDO"></img> --- {m.Year}</div>  ) }
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const data = await fetchData();
+
+  return {
+    props: data,
+  };
+}
+
+
+export default Films;
